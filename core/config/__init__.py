8d0ff33f0290079ea9452ae17ba7665fc5b7b53f
Janus Protocol <cryptodm@outlook.com>
2025-10-20 18:08:23 -0300

    Create __init__.py

---
+++ core/config/__init__.py
@@ -0,0 +1,3 @@
+# config package (Fase 12)
+from .types import JanusConfig, NetworkProfile, PolicyConfig, ObservabilityConfig, BridgeConfig
+from .loader import load_config, load_profile, env_overrides
