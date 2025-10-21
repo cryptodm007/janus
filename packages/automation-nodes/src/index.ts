c2892365817b28adad7131456cb4198e6edee11a
Janus Protocol <cryptodm@outlook.com>
2025-10-21 16:24:03 -0300

    Add exports for automation nodes
    Exports various nodes for n8n to locate.

---
+++ packages/automation-nodes/src/index.ts
@@ -0,0 +1,6 @@
+// Exporta os nodes para o n8n localizar.
+export * from './nodes/BaseTransactionBuilder.node';
+export * from './nodes/BaseTransactionExecutor.node';
+export * from './nodes/SolanaTransactionBuilder.node';
+export * from './nodes/SolanaTransactionExecutor.node';
+export * from './nodes/OnChainEventTrigger.node';
